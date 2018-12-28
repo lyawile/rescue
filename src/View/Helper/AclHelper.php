f3be08976b421f695d095a262f947777e7857b58
robby <0881londo@gmail.com>
2018-12-28 16:56:54 +0300

    Fixes Acl helper

--- src/View/Helper/AclHelper.php
+++ src/View/Helper/AclHelper.php
@@ -43,7 +43,7 @@ class AclHelper extends Helper
         $collection = new ComponentRegistry();
         $this->Acl = new AclComponent($collection);
         $this->Authorize = new ActionsAuthorize($collection);
-        $this->Authorize->getConfig($this->getConfig());
+        $this->Authorize->getConfig();
     }
 
     /**
