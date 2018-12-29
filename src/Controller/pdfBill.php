2a8afe268700fc787d712319321041cc809890ba
Hassan Lyawile <webdev271@gmail.com>
2018-12-29 09:14:43 +0300

    Text rotation methods added

--- src/Controller/pdfBill.php
+++ src/Controller/pdfBill.php
@@ -7,8 +7,10 @@ namespace App\Controller;
  *
  * @author Admin
  */
-class pdfBill extends \FPDF{
-    
+class pdfBill extends \FPDF {
+
+    var $angle = 0;
+
     public function Header() {
         parent::Header();
         $this->SetFont('Arial', 'B', 16);
@@ -30,9 +32,9 @@ class pdfBill extends \FPDF{
         $this->Cell(45);
         $this->Cell(0, 7, "Telephone: +255-22-2700493 - 6/9, Email: esnecta@necta.go.tz", 0, 1);
         // Push the contents down before starting the body by 15mm
-        $this->Cell(0,15, "",0,1);
-
+        $this->Cell(0, 15, "", 0, 1);
     }
+
     public function Footer() {
         parent::Footer();
         $this->AliasNbPages('{totalPages}');
@@ -41,4 +43,31 @@ class pdfBill extends \FPDF{
         $this->SetFont('Arial');
         $this->Cell(0, 5, "Page " . $this->PageNo() . "/{totalPages}", 0, 1);
     }
+
+    function Rotate($angle, $x = -1, $y = -1) {
+        if ($x == -1)
+            $x = $this->x;
+        if ($y == -1)
+            $y = $this->y;
+        if ($this->angle != 0)
+            $this->_out('Q');
+        $this->angle = $angle;
+        if ($angle != 0) {
+            $angle *= M_PI / 180;
+            $c = cos($angle);
+            $s = sin($angle);
+            $cx = $x * $this->k;
+            $cy = ($this->h - $y) * $this->k;
+            $this->_out(sprintf('q %.5F %.5F %.5F %.5F %.2F %.2F cm 1 0 0 1 %.2F %.2F cm', $c, $s, -$s, $c, $cx, $cy, -$cx, -$cy));
+        }
+    }
+
+    function _endpage() {
+        if ($this->angle != 0) {
+            $this->angle = 0;
+            $this->_out('Q');
+        }
+        parent::_endpage();
+    }
+
 }
